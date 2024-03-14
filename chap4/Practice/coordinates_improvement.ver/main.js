98a28e827ed3f7c8c2dbf1fd4e339113cfaa0aa3
mikan <joooong1021@gmail.com>
2024-03-14 15:53:03 +0900

    fix: 좌표 실습 성능 개선

---
+++ chap4/Practice/coordinates_improvement.ver/main.js
@@ -0,0 +1,20 @@
+const vertical = document.querySelector(".vertical");
+const horozontal = document.querySelector(".horozontal");
+const target = document.querySelector(".target");
+const tag = document.querySelector(".tag");
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
+
+document.addEventListener("mousemove", (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  vertical.style.transform = `translateX(${x}px)`;
+  horozontal.style.transform = `translateY(${y}px)`;
+  target.style.transform = `translate(${x - targetHalfWidth}px, ${
+    y - targetHalfHeight
+  }px)`;
+  tag.style.transform = `translate(${x + 20}px, ${y + 20}px)`;
+  tag.innerHTML = `${x}px, ${y}px`;
+});
