ad23f46fb5eea840c36624df2e63c30cad1020df
mikan <joooong1021@gmail.com>
2024-03-25 18:14:41 +0900

    fix: 실습8 당근 게임5

--- chap8/Practice/carrot/main.js
+++ chap8/Practice/carrot/main.js
@@ -2,6 +2,7 @@ const btnPlay = document.querySelector(".button__play");
 const boardTimer = document.querySelector(".board__timer");
 const boardScore = document.querySelector(".board__score");
 const cabinetBoard = document.querySelector(".cabinet__win_lost_board");
+const btnRetry = document.querySelector(".button__retry");
 
 let audioBgm = new Audio("sound/bg.mp3");
 let timer;
@@ -9,14 +10,20 @@ let sec = 10;
 
 btnPlay.addEventListener("click", (e) => {
   if (e.target.classList.contains("btn-start")) {
-    audioBgm.play();
+    // audioBgm.play();
     btnPlay.innerHTML = '<i class="fa-solid fa-stop btn-stop"></i>';
     boardTimer.innerText = `00:${sec}`;
     timer = setInterval(counterTimer, 1000);
+    setTimeout(function () {
+      audioBgm.pause();
+    }, 10000);
   } else {
     btnPlay.innerHTML = '<i class="fa-solid fa-play btn-start"></i>';
     boardTimer.innerText = `00:${sec}`;
-    cabinetBoard.style.display = "block";
+    cabinetBoard.style.visibility = "visible";
+    btnRetry.addEventListener("click", (e) => {
+      // 눌렀을 떄 게임 초기화 !
+    });
     clearInterval(timer);
   }
 });
