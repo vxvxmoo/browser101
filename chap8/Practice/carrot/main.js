ba79626ed50e6f7a7cc2a65c9c971cd8ac57f7c0
mikan <joooong1021@gmail.com>
2024-03-25 17:39:54 +0900

    fix: 실습8 당근 게임4

--- chap8/Practice/carrot/main.js
+++ chap8/Practice/carrot/main.js
@@ -1,18 +1,22 @@
 const btnPlay = document.querySelector(".button__play");
 const boardTimer = document.querySelector(".board__timer");
 const boardScore = document.querySelector(".board__score");
+const cabinetBoard = document.querySelector(".cabinet__win_lost_board");
 
+let audioBgm = new Audio("sound/bg.mp3");
 let timer;
 let sec = 10;
 
 btnPlay.addEventListener("click", (e) => {
   if (e.target.classList.contains("btn-start")) {
+    audioBgm.play();
     btnPlay.innerHTML = '<i class="fa-solid fa-stop btn-stop"></i>';
     boardTimer.innerText = `00:${sec}`;
     timer = setInterval(counterTimer, 1000);
   } else {
     btnPlay.innerHTML = '<i class="fa-solid fa-play btn-start"></i>';
     boardTimer.innerText = `00:${sec}`;
+    cabinetBoard.style.display = "block";
     clearInterval(timer);
   }
 });
@@ -24,5 +28,5 @@ function counterTimer() {
     return;
   }
   sec--;
-  boardTimer.innerText = `00:0${sec}`;
+  boardTimer.innerText = `00:${sec}`;
 }
