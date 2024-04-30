b39599bc7af87301c181c40f1f5a9a318ebad614
mikan <joooong1021@gmail.com>
2024-04-30 12:05:45 +0900

    fix: 실습 8 리팩토링_함수를 깔끔하게

--- chap9/Practice/carrot_teacher_renewal/main.js
+++ chap9/Practice/carrot_teacher_renewal/main.js
@@ -2,6 +2,7 @@
 
 import PopUp from "./popup.js";
 import { GameBuilder, Reason } from "./game.js";
+import * as sound from "./sound.js";
 
 const gameFinishBanner = new PopUp();
 
@@ -15,12 +16,15 @@ game.setGameStopListener((reason) => {
   switch (reason) {
     case Reason.cancel:
       message = "Replay❓";
+      sound.playAlert();
       break;
     case Reason.win:
       message = "YOU WON 🎉";
+      sound.playWin();
       break;
     case Reason.lose:
       message = "YOU LOST 💩";
+      sound.playBug();
       break;
     default:
       throw new Error("not valid reason");
